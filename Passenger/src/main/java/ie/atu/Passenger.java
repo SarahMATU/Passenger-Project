e4e925bd600c2513803a791b31d2ff91e22267a3
Sarah Mitchell <g0391704@atu.ie>
2022-10-31 13:27:19 +0000

    Cleaned up Code and added Comments

--- Passenger/src/main/java/ie/atu/Passenger.java
+++ Passenger/src/main/java/ie/atu/Passenger.java
@@ -1,3 +1,6 @@
+//Name: Sarah Mitchell
+//No: G00391704
+//Description: Passenger Project CI/CD
 package ie.atu;
 
 public class Passenger {
@@ -8,79 +11,62 @@ public class Passenger {
         private String numPhone;
         private int numAge;
 
-
-        public Passenger(){
+        public Passenger(){ //Default Constructor
 
         }
 
-        public String setUserTitle(String title) {
-                if(title.equals("Mr")||title.equals("Mrs")||title.equals("Ms"))
-                {
+        public String setUserTitle(String title) { //Sets the Users Title
+                if(title.equals("Mr")||title.equals("Mrs")||title.equals("Ms")) {
                         userTitle = title;
                 }
-                else
-                {
+                else {
                         throw new IllegalArgumentException("This is not a valid Title");
                 }
 
                 return userTitle;
         }
 
-        public String setUserName(String name)
-        {
-                if(name.length() > 3 )
-                {
+        public String setUserName(String name) { //Sets the Users Name
+                if(name.length() > 3 ) {
                         userName = name;
                 }
 
-                else
-                {
+                else { //If the name is shorter than 3 Characters, the program throws an error.
                         throw new IllegalArgumentException("This is not a valid Name");
                 }
                 return userName;
         }
 
-        public String setUserId(String id)
-        {
-                if(id.length() > 10 )
-                {
+        public String setUserId(String id) { //Sets the Users ID Number
+                if(id.length() > 10 ) {
                         userId = id;
                 }
 
-                else
-                {
+                else { //If the ID is shorter than 10 Characters, the program throws an error.
                         throw new IllegalArgumentException("This is not a valid ID");
                 }
                 return userId;
         }
 
-        public String setNumPhone(String phone)
-        {
-                if(phone.length() > 7
-                )
-                {
+        public String setNumPhone(String phone) { //Sets the Users Phone Number
+                if(phone.length() > 7) {
                         numPhone = phone;
                 }
 
-                else
-                {
+                else { //If the Phone Number is shorter than 7 Characters, the program throws an error.
                         throw new IllegalArgumentException("This is not a valid Phone Number");
                 }
                 return numPhone;
         }
 
-        public int setNumAge(int age)
-        {
-                if(age > 16)
-                {
+        public int setNumAge(int age) { //Sets the Users Age
+                if(age > 16) {
                         numAge = age;
                 }
 
-                else
-                {
+                else { //If the Users age is less than 16, the program throws an error.
                         throw new IllegalArgumentException("This is not a valid Phone Number");
                 }
                 return numAge;
         }
-
 }
