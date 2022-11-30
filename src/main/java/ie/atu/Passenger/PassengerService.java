04368b3fe54eb272067f675e9bc47fb9ce9b68d3
Sarah Mitchell <G00391704@atu.ie>
2022-11-30 12:35:50 +0000

    Added Code to connect to the Database

--- src/main/java/ie/atu/Passenger/PassengerService.java
+++ src/main/java/ie/atu/Passenger/PassengerService.java
@@ -1,23 +1,20 @@
 package ie.atu.Passenger;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
-
+@AllArgsConstructor
 @Service
 public class PassengerService {
 
-
+private final PassengerRepo passengerRepo;
 
     public List <Passenger> getPassenger ()
     {
-        List<Passenger> myPassenger = List.of(
-                new Passenger("Mrs", "Sarah", "3E7F9N2Y1H4T", "0895746178", 30),
-                new Passenger("Mrs", "Rayah", "3E7F9U9Y1H4T", "0895734578", 25),
-                new Passenger("Mrs", "Brittany", "3E7F9N2Y6W4T", "0894827178", 65));
-        return myPassenger;
+        return passengerRepo.findAll();
     }
 
 
