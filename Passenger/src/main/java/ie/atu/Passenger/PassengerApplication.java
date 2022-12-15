b08a1b1f53d2e2be50fa86e3f4f522ae910eb63a
sarah mitchell <g00391704@atu.ie>
2022-12-15 16:48:49 +0000

    Added dockerfile(Not finished)

--- Passenger/src/main/java/ie/atu/Passenger/PassengerApplication.java
+++ Passenger/src/main/java/ie/atu/Passenger/PassengerApplication.java
@@ -3,6 +3,7 @@ package ie.atu.Passenger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RequestMapping(path = "api/passenger")
 public class PassengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerApplication.class, args);
+	}
 
 	PassengerService myService;
 
@@ -21,11 +26,6 @@ public class PassengerApplication {
 	public PassengerApplication(PassengerService myService)
 	{
 		this.myService = myService;
-	}
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(PassengerApplication.class, args);
 	}
 
 	@GetMapping
